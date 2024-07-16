bb8f298d4b3064acdf0608a0a7e4807387b4e83b
bmille86 <colecomic06@gmail.com>
2024-07-16 14:29:50 -0400

    Update japan.js

--- japan.js
+++ japan.js
@@ -1,6 +1,6 @@
 // 36.2048° N, 138.2529° E -> 36.2048, 138.2529
 let map = L.map("map", {
-  center: [36.2048, 138.2529],
+  center: [35.6586, 139.7454],
   zoom: 15
 });
 
@@ -10,7 +10,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // tokyo tower: Coordinates: 35.6586° N, 139.7454° E -> 35.6586, 35.6586
-L.circle([35.6586, 35.6586], {
+L.circle([35.6586, 139.7454], {
          fillcolor: 'teal',
           color: 'blue',
           radius: 1000

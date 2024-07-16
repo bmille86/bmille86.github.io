30471813fc23d7fbbd8505e8d177d8d665a6f31e
bmille86 <colecomic06@gmail.com>
2024-07-16 14:16:55 -0400

    Update japan.js

--- japan.js
+++ japan.js
@@ -8,3 +8,10 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+// tokyo tower: Coordinates: 35.6586° N, 139.7454° E -> 35.6586, 35.6586
+L.circle([35.6586, 35.6586], {
+         fillcolor: 'teal',
+          color: 'blue',
+          radius: 1000
+}).addTo(map);
